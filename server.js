cc2e4f13dc06f78efba596399897d1d66e461dd4
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2021-01-11 23:07:41 -0600

    :hammer: Testing db key with space

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ app.post('/image-upload', async (req, res) => {
   const { bikeID, bikeDetails } = req.body;
   const values = Object.values(req.files)
   const parsedBikeDetails = JSON.parse(bikeDetails);
-  console.log({parsedBikeDetails})
-  const { bikeModel } = parsedBikeDetails;
+  const { ['Bike Model']: bikeModel } = parsedBikeDetails;
 
   const promises = values.map(image => {
     return cloudinary.uploader.upload(image.path, {public_id: req.public_id})
